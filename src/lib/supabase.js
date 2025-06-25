8d4e73de57827b1eb542a947dc04f00f3adafc94
dmccolly <128931847+dmccolly@users.noreply.github.com>
2025-06-25 08:38:41 -0700

    Update supabase.js

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,4 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
+console.log('VITE_SUPABASE_URL:', import.meta.env.VITE_SUPABASE_URL);
+console.log('VITE_SUPABASE_ANON_KEY:', import.meta.env.VITE_SUPABASE_ANON_KEY);
 
 export const supabase = createClient(
 import.meta.env.VITE_SUPABASE_URL ?? process.env.SUPABASE_URL,
