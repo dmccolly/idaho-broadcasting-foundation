a3bd3d8efab8d2ec787b5cb9e5e191becbe59e5f
dmccolly <128931847+dmccolly@users.noreply.github.com>
2025-06-26 12:36:02 -0700

    Update NewsSocialFeed.jsx

--- src/components/NewsSocialFeed.jsx
+++ src/components/NewsSocialFeed.jsx
@@ -134,7 +134,7 @@ const NewsSocialFeed = () => {
  
       
       {/* Post Creation */}
-      <div className="bg-white pp-4roup-4 rounded-lg-md border">
+      <div className=bg-white p-4 rounded-lg shado--md  bordder">
         <h3 className="text-xl font-semibold mb-4">Share an Update</h3>
         <div className="space-y-4">
           <div>
