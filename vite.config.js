f850957ccf8e8e41284fffbff8bb63c3f42f0924
dmccolly <128931847+dmccolly@users.noreply.github.com>
2025-06-28 14:02:29 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-import path from 'path'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-    server: {
-    host: true,
-    allowedHosts: [".netlify.app"],
-  },
+  plugins: [react()],
+  base: './'
 })
