030a859b65d1f59a0838b4f92439075198a5929f
dmccolly <128931847+dmccolly@users.noreply.github.com>
2025-07-05 14:49:32 -0400

    Update init.js

--- public/editor/init.js
+++ public/editor/init.js
@@ -1,10 +1,10 @@
 window.CMS_MANUAL_INIT = true;
 
 (function () {
-  var cms = window.CMS || window.DecapCmsApp || window.DecapCms;
-  if (cms) {
-    cms.init({ config: '/editor/config.yml' });
-  } else {
-    console.error('Decap CMS global not found.');
-  }
+    var cms = window.CMS || window.DecapCmsApp || window.DecapCms;
+    if (cms) {
+          cms.init({ config: '/editor/config.yml' });
+    } else {
+          console.error('Decap CMS global not found.');
+    }
 })();
