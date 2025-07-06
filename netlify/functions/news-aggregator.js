62a9f4306ca537daad8b5e75375e37f6db837610
dmccolly <128931847+dmccolly@users.noreply.github.com>
2025-07-06 15:59:24 -0700

    Clarify ES module usage in news aggregator

--- netlify/functions/news-aggregator.js
+++ netlify/functions/news-aggregator.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
 import { parse } from 'node-html-parser';
+// Note: ES module syntax is required since package.json uses "type": "module"
+
 
 // Configuration for news sources
 const NEWS_SOURCES = {
