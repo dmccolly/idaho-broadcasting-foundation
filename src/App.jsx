0f2087e07dac035715780eaba761fab8a2e203d0
dmccolly <128931847+dmccolly@users.noreply.github.com>
2025-07-10 15:25:24 -0600

    ignore build output

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,6 @@ import { BrowserRouter as Router, Routes, Route, Link, useLocation } from 'react
 import AdminDashboard from './components/AdminDashboard';
 import EventsPage from './components/EventsPage';
 import EventsManager from './components/EventsManager';
-import VoxProManagement from './components/VoxProManagement';
-import VoxProPlayer from './components/VoxProPlayer';
 
 // Main Layout Component
 const Layout = ({ children }) => {
@@ -261,8 +259,6 @@ function App() {
           <Route path="/news" element={<NewsPage />} />
           <Route path="/admin" element={<AdminDashboard />} />
           <Route path="/admin/events" element={<EventsManager />} />
-          <Route path="/admin/voxpro-player" element={<VoxProPlayer />} />
-          <Route path="/admin/voxpro-management" element={<VoxProManagement />} />
         </Routes>
       </Layout>
     </Router>
